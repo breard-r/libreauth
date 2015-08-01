0116b2e3c8b56414850c4fd9a40bd7bdbb1bebe7
Rodolphe Breard <rodolphe@what.tf>
2015-08-01 22:25:55 +0200

    Adding the readme description to the crate documentation. This is more friendly for users accessing the documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,31 @@
 // OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 //
 
+//!
+//! [![Build Status](https://api.travis-ci.org/breard-r/r2fa.svg?branch=master)](https://travis-ci.org/breard-r/r2fa)
+//! [![R2FA on crates.io](https://img.shields.io/crates/v/r2fa.svg)](https://crates.io/crates/r2fa)
+//! [![R2FA on GitHub](https://img.shields.io/github/license/breard-r/r2fa.svg)](https://github.com/breard-r/r2fa)
+//!
+//! Rust Two-Factor Authentication (R2FA) is a collection of tools for two-factor authentication.
+//!
+//!
+//! ## Features
+//! - HOTP - HMAC-based One-time Password Algorithm ([RFC 4226](https://tools.ietf.org/html/rfc4226))
+//!   - the key can be passed as bytes, an ASCII string, an hexadicimal string or a base32 string
+//!   - customizable counter
+//!   - customizable hash function (sha1, sha256, sha512)
+//!   - customizable output length
+//!   - customizable output alphabet
+//! - TOTP - Time-based One-time Password Algorithm ([RFC 6238](https://tools.ietf.org/html/rfc6238))
+//!   - the key can be passed as bytes, an ASCII string, an hexadicimal string or a base32 string
+//!   - customizable timestamp
+//!   - customizable period
+//!   - customizable initial time (T0)
+//!   - customizable hash function (sha1, sha256, sha512)
+//!   - customizable output length
+//!   - customizable output alphabet
+//!
+
 extern crate rustc_serialize;
 extern crate base32;
 extern crate crypto;
